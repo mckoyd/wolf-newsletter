ec44c6c16b469eda62bc23b417a94c5ed75d2c96
mckoyd <dominick.mckoy@gmail.com>
2024-05-06 15:50:13 -0400

    Updates

--- src/config/ThankYouPage.config.ts
+++
@@ -1,6 +0,0 @@
-export const thankYouPageText = {
-  thankYou: "Thanks for subscribing!",
-  confirmation:
-    "A confirmation email has been sent to ash@loremcompany.com. Please open it and click the button inside to confirm your subscription.",
-  buttonText: "Dismiss message",
-};
--- src/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-export interface IUpdate {
-  id: string;
-  update: string;
-}
